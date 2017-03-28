6af78e9fd90c78b9eaee767b3cfb34982142efa1
Patrick Steele-Idem <pnidem@gmail.com>
2017-03-28 17:16:25 -0600

    Fixed Vue

--- benchmarks/search-results/vue/rollup.config.js
+++ benchmarks/search-results/vue/rollup.config.js
@@ -1,9 +1,8 @@
 import commonjsPlugin from 'rollup-plugin-commonjs';
-import browserifyPlugin from 'rollup-plugin-browserify-transform';
 import nodeResolvePlugin from 'rollup-plugin-node-resolve';
 import babelPlugin from 'rollup-plugin-babel';
-import envify from 'envify';
 import path from 'path';
+import replace from 'rollup-plugin-replace';
 
 export default {
     entry: path.join(__dirname, 'client.jsx'),
@@ -24,6 +23,10 @@ export default {
         commonjsPlugin({
             include: [ 'node_modules/**', '**/*.js', '**/*.jsx'],
             extensions: [ '.js', '.jsx' ]
+        }),
+        replace({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+          'process.env.VUE_ENV': JSON.stringify('browser')
         })
     ],
     dest: path.join(process.env.BUNDLES_DIR, 'vue.js')
