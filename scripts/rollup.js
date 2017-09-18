5567501fd8e7e75c6f16220a560db8cc3e0d64d3
Patrick Steele-Idem <pnidem@gmail.com>
2017-09-18 17:56:16 -0600

    Improved build scripts

--- scripts/rollup.js
+++ scripts/rollup.js
@@ -1,19 +1,21 @@
 require('../init');
 
 var execSync = require('child_process').execSync;
+var path = require('path');
 
 var benchmarks = require('../benchmarks');
 var targetLib = process.argv[2];
 
 benchmarks.forEach((benchmark) => {
+    var benchmarkName = benchmark.name;
+
     benchmark.benches.forEach((bench) => {
         var libName = bench.name;
         if (targetLib && libName !== targetLib) {
             return;
         }
 
-        console.log('------');
-        console.log(`Bundling ${libName}...`);
-        execSync(`npm run bundle-${libName}`);
+        process.env.BUNDLES_DIR = path.join(__dirname, `../build/bundles/${benchmarkName}`);
+        execSync(`rollup -c ${bench.dir}/rollup.config.js`);
     });
 });
